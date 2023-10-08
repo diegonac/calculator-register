5538c06e2abd64e1f58776ef49fb612b61b23a98
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-10-08 20:15:55 -0300

    feat: added question when leaving the page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,6 +6,16 @@ import useSumsContext from "../hooks/useSumsContext";
 const Layout: React.FC = () => {
   const {order} = useSumsContext();
 
+  const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+    const message = '¿Estás seguro de que quieres abandonar la página?';
+    event.returnValue = message;
+    return message;
+  };
+
+  if(order.sums.length === 1) {
+    window.addEventListener('beforeunload', handleBeforeUnload);
+  }
+
   return (
     <main className="flex flex-col items-center min-h-screen min-w-320">
       <h1 className="my-6 text-2xl">Calculadora registradora</h1>
