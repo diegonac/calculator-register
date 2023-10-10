acfc43075bcbf264adc5993b3a76f3682ddb3d6a
Diego Nacimiento <diegonacimiento18@gmail.com>
2023-10-10 14:14:33 -0300

    feat: delete modal

--- src/modals/Modal.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import { ReactNode } from "react";
-
-const Modal = ({ children }: { children: ReactNode }) => (
-  ReactDOM.createPortal(children, document.getElementById("modal")!)
-);
-
-export default Modal;
